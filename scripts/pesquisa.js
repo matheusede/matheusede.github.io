8d4ceaf461898e6d6fa18d39e40f59c545a4de60
Matheus Marcolino <matheuseduardoleitemarcolino@gmail.com>
2021-06-18 21:29:20 -0300

    v3.0

--- scripts/pesquisa.js
+++ scripts/pesquisa.js
@@ -21,17 +21,19 @@ function exibeFilmes ()
 
     for (let i = 0; i < data.results.length; i++) {
         let nomeFilme = data.results[i].title;
-        let sinopse = data.results[i].overview;
-        let imagem = IMG + data.results[i].poster_path;
+        let lancamento = data.results[i].release_date;
+        let nota = data.results[i].vote_average;
+        let imagem = IMG_PREFIX + data.results[i].poster_path;
 
-        textoHTML += `<div class="card col-md-4">
-            <img src="${imagem}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${nomeFilme}</h5>
-                <p class="card-text">${sinopse}</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>`
+        textoHTML += `<div class=" mx-auto card col-12 col-sm-12 col-md-3 col-lg-3">
+        <img src="${imagem}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${nomeFilme}</h5>
+            <p>Data de Lançamento: ${lancamento}</p>
+            <p>Nota: ${nota} <i class="fas fa-star"></i></p>
+            <a href="https://www.themoviedb.org/movie/${data.results[i].id}" class="btn btn-primary">Saiba Mais</a>
+        </div>
+    </div>`
     }
 
     document.getElementById('boxPesquisa').innerHTML = textoHTML;
